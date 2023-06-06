9646abc5ace0dea7446a4a845e7ee0990f1c5041
jorgerozop <121322400+jorgerozop@users.noreply.github.com>
2023-06-05 22:12:53 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
+var score =0;
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
+var fruitGroup;
+var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
+var player_img;
+var player1score =0;
+var player2score =0;
+
+function preload(){
+  back_img = loadImage("images/fondo.png");
+  player_img = loadImage("images/basket2.png");
+  fruit1_img = loadImage("images/manzana.png");
+  fruit2_img = loadImage("images/banano.png");
+  fruit3_img = loadImage("images/melon2.png");
+  fruit4_img = loadImage("images/naranja.png");
+  fruit5_img = loadImage("images/piña.png");
+  fruitGroup = new Group();
+}
+function setup() {
+  createCanvas(1000, 600);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw() {
+  background(back_img);
+  
+   if (playerCount === 2) {
+     game.update(1);
+   }
+   if (gameState === 1) {
+     clear(); 
+     game.play();
+   }
+   if (gameState === 2) {
+   
+  }
+}
